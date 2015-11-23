de25b9aedf08d7eb01f4e2205a2f4919863c2a9c
qianfeng <qianfeng@qianfengdemac-mini.local>
2015-11-23 14:52:05 +0800

    v1

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright © 2015年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright © 2015年 JW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
